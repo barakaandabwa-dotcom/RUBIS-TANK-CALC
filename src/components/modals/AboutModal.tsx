f75b6aaaa38b72fe59079469c1a403c80ba806b4
barakaandabwa-dotcom <barakaandabwa@gmail.com>
2025-08-23 05:55:18 -0700

    Update about section with new tank details

--- src/components/modals/AboutModal.tsx
+++ src/components/modals/AboutModal.tsx
@@ -8,23 +8,19 @@ export function AboutModal({ open, onOpenChange }: { open: boolean; onOpenChange
           <DialogTitle>About / Calibration</DialogTitle>
         </DialogHeader>
         <div className="space-y-1 text-sm">
-          <p>Client: Swahili Beach Hotel</p>
-          <p>Revision Date: 30th July, 2025</p>
-          <p>Revision No.: rev 01</p>
-          <p>Project No.: 20257001032TC</p>
-          <p>Tank: 0160750</p>
-          <p>Tank Owner: Swahili Beach Hotel</p>
-          <p>Location: Diani Beach, Kenya</p>
+          <p>Tank: Tank 01</p>
+          <p>Tank Owner: Rubis, Zambia.</p>
+          <p>Location: Zambia</p>
           <p>Tank Description: LPG Bullet Tank</p>
-          <p>Nominal Diameter: 1190 mm</p>
-          <p>Cylinder Length: 3700 mm</p>
-          <p>Tank Nominal Capacity: 3898 Liters</p>
-          <p>Date of Calibration: 30/07/2025</p>
+          <p>Nominal Diameter: 2130 mm</p>
+          <p>Cylinder Length: 11600 mm</p>
+          <p>Tank Nominal Capacity: 39557 Liters</p>
+          <p>Date of Calibration: 29/07/2025</p>
           <p>Validity: 10 Years</p>
-          <p>Overall Uncertainty: +0.012%</p>
+          <p>Overall Uncertainty: +0.0113%</p>
           <p>Method of Calibration: API MPMS CHAPTER 2</p>
           <p>Tank calibrated by: Murban Engineering Limited</p>
-          <p>Certificate No.: 20257001032TC-0160750</p>
+          <p>Certificate No.: 20257001039TC-01</p>
         </div>
       </DialogContent>
     </Dialog>
